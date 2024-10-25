b83a65e7a86e2287928c1300628fdf2ccdd5aa0e
cong <congco@foxmail.com>
2024-10-25 17:36:06 +0800

    feat: gaode

--- app_click.py
+++ app_click.py
@@ -7,22 +7,46 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from typing import List
+
+from ialgo import TravelingSalesman
+from ialgo import AmapDriving
+from ialgo import iCenterline
 
 app = FastAPI()
 
+
+class iloc(BaseModel):
+    lat: float
+    lng: float
+
+
 class Location(BaseModel):
-    latitude: float
-    longitude: float
+    pts: List[iloc]
+
 
 @app.post("/location/")
-async def receive_location(location: Location = Body(...)):
-    res = {"latitude": location.latitude, "longitude": location.longitude}
-    print(res)
+async def receive_location(location: Location):
+    # 处理前端传入的坐标点
+    pnts = [{"lat": _.lat, "lng": _.lng} for _ in location.pts]
+    otravel = TravelingSalesman(pnts)
+    pnts = otravel.find_shortest_path()  # 点排序
+    path = []  # 高德规划路径 多段
+    for fp, tp in zip(pnts[:-1], pnts[1:]):
+        oAmap = AmapDriving(fp, tp)
+        amap_path = oAmap.get_driving_route(fp, tp)
+        path.append(amap_path)
+    path = ';'.join(path)  # 高德路径拼接
+    path = [eval(_) for _ in path.split(';')]
+    oline = iCenterline(path)  # 计算中心线 防止回头路
+    res = oline.iSampleLine()  # 格式同 Location
     return res
+
 
 @app.get("/")
 async def iclick_html():
-    return FileResponse('iclick.html')
+    return FileResponse('templates/pathBaseMultiPoint.html')
+
 
 if __name__ == "__main__":
     import uvicorn
---
+++ ialgo.py
@@ -0,0 +1,100 @@
+# -*- coding:utf-8 -*-
+"""
+@Author:ddz
+@Date:2024/10/24 10:07
+@Project:ialgo
+"""
+import itertools
+from geopy.distance import geodesic
+from shapely.geometry import LineString
+from pygeoops import centerline
+import geopandas as gpd
+import requests
+
+# from ilog import logger
+
+
+class TravelingSalesman:
+    # 旅行商问题
+    # 输入：经纬度点列表
+    # 输出：排序好的点列表
+    def __init__(self, points):
+        self.points = points  # 经纬度点列表
+        self.min_distance = float('inf')  # 最小距离初始化为无穷大
+        self.best_path = []  # 最优路径
+
+    def calculate_distance(self, point1, point2):
+        """计算两点间的欧几里得距离"""
+        return geodesic(point1[::-1], point2[::-1]).m
+
+    def find_shortest_path(self):
+        """找到旅行商问题的最短路径"""
+        # 生成所有可能的路径
+        for path in itertools.permutations(self.points):
+            distance = self.calculate_total_distance(path)
+            if distance < self.min_distance:
+                self.min_distance = distance
+                self.best_path = path
+
+        return self.best_path
+
+    def calculate_total_distance(self, path):
+        """计算给定路径的总距离"""
+        total_distance = 0
+        for i in range(len(path) - 1):
+            total_distance += self.calculate_distance(path[i], path[i + 1])
+
+        return total_distance
+
+
+class iCenterline:
+    def __init__(self, points, crs='epsg:4525'):
+        # points 经纬度点列表
+        self.gps = gpd.GeoSeries(LineString(points), crs=4326).to_crs(crs)
+        self.crs = crs
+
+    def iSampleLine(self):
+        sline = self.gps.geometry.values[0]
+        sline = sline.buffer(30).buffer(-15)
+        cline = centerline(sline)
+        gps = gpd.GeoSeries(cline, crs=self.crs).to_crs(4326)
+        gps.to_file('vector/cline.geojson', driver='GeoJSON')
+        return [{'lng': lng, 'lat': lat} for lng, lat in gps.geometry.values[0].coords]
+
+
+class AmapDriving:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.url = "https://restapi.amap.com/v3/direction/driving"
+
+    def get_driving_route(self, origin, destination):
+        params = {
+            'key': self.api_key,
+            'origin': origin,         # 起点经纬度，格式为"经度,纬度"
+            'destination': destination # 终点经纬度，格式为"经度,纬度"
+        }
+        response = requests.get(self.url, params=params)
+        route_info = response.json()
+        if route_info.get('status') == '1':
+            path_segments = route_info.get('route').get('paths')[0].get('steps')
+            path = []
+            for ipath in path_segments:
+                path.append(ipath.get('polyline'))
+            path = ';'.join(path)
+        else:
+            path = ''
+
+        print("驾车路线信息:", path)
+        return path
+
+
+if __name__ == "__main__":
+    # 输入一些经纬度点
+    points = [(34.89042, 113.4074), (34.82304, 113.4737), (34.80522, 113.2437), (34.8566, 113.23522)]
+    points = [_[::-1] for _ in points]
+    tsp = TravelingSalesman(points)
+    best_path = tsp.find_shortest_path()
+    cline = iCenterline(best_path)
+    sline = cline.iSampleLine()
+    print("最短路径:", best_path)
+
