8bc9c018caa58fd8185c9d126cd3f4b40a824302
cong <congco@foxmail.com>
2024-10-28 14:03:01 +0800

    feat: gaode

--- ialgo.py
+++ ialgo.py
@@ -10,6 +10,9 @@ from shapely.geometry import LineString
 from pygeoops import centerline
 import geopandas as gpd
 import requests
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # from ilog import logger
 
@@ -23,9 +26,29 @@ class TravelingSalesman:
         self.min_distance = float('inf')  # 最小距离初始化为无穷大
         self.best_path = []  # 最优路径
 
+    def calculate_distance2(self, point1, point2):
+        """计算两点间的欧几里得距离"""
+        print(point1[::-1], point2[::-1])
+        return geodesic(point1[::-1], point2[::-1]).m
+
     def calculate_distance(self, point1, point2):
         """计算两点间的欧几里得距离"""
-        return geodesic(point1[::-1], point2[::-1]).m
+        # 检查输入参数是否为元组或列表
+        if not (isinstance(point1, (tuple, list)) and isinstance(point2, (tuple, list))):
+            raise ValueError("Points must be tuples or lists")
+
+        # 检查每个点是否包含两个元素
+        if len(point1) != 2 or len(point2) != 2:
+            raise ValueError("Each point must contain exactly two elements (latitude, longitude)")
+
+        # 计算距离
+        distance = geodesic(point1, point2).m
+
+        # 记录日志
+        logging.debug(f"Calculating distance between {point1} and {point2}")
+        logging.debug(f"Distance: {distance} meters")
+
+        return distance
 
     def find_shortest_path(self):
         """找到旅行商问题的最短路径"""
@@ -70,8 +93,8 @@ class AmapDriving:
     def get_driving_route(self, origin, destination):
         params = {
             'key': self.api_key,
-            'origin': origin,         # 起点经纬度，格式为"经度,纬度"
-            'destination': destination # 终点经纬度，格式为"经度,纬度"
+            'origin': origin,  # 起点经纬度，格式为"经度,纬度"
+            'destination': destination  # 终点经纬度，格式为"经度,纬度"
         }
         response = requests.get(self.url, params=params)
         route_info = response.json()
@@ -97,4 +120,3 @@ if __name__ == "__main__":
     cline = iCenterline(best_path)
     sline = cline.iSampleLine()
     print("最短路径:", best_path)
-
